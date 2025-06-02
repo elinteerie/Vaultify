79af4a4c0d9ca1a273a111d4766910abb574e9f5
Emmanuelobakpolor <emmanuelobakpolor6655@gmail.com>
2025-06-02 16:08:29 +0100

    commit

--- accounts/migrations/0010_alert.py
+++ accounts/migrations/0010_alert.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0009_merge_20250522_2250'),
+      
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
