bcd414744a20cf941f8f0323935bf137c0012ef5
Emmanuelobakpolor <emmanuelobakpolor6655@gmail.com>
2025-06-02 15:14:09 +0100

    Configured PostgreSQL on Render

--- vaultify/wsgi.py
+++ vaultify/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vaultify.settings')
+
+# Setup Django
+django.setup()
+
+# Run migrations automatically
+from django.core.management import call_command
+try:
+    call_command('migrate')
+except Exception as e:
+    print(f"Migration failed: {e}")
 
 application = get_wsgi_application()
