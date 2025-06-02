2e2a65f411165f98d4c7b1a22a99ba7d2882a344
Emmanuelobakpolor <emmanuelobakpolor6655@gmail.com>
2025-06-02 18:17:26 +0100

    commit

--- vaultify/settings.py
+++ vaultify/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-import dj_database_url
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,9 +127,11 @@ LOGGING = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 ROOT_URLCONF = 'vaultify.urls'
