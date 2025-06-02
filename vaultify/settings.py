54b3e7def91eff2b68cfa70ae366a62391b5aeef
Emmanuelobakpolor <emmanuelobakpolor6655@gmail.com>
2025-06-02 15:08:46 +0100

    Configured PostgreSQL on Render

--- vaultify/settings.py
+++ vaultify/settings.py
@@ -127,11 +127,10 @@ LOGGING = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 ROOT_URLCONF = 'vaultify.urls'
